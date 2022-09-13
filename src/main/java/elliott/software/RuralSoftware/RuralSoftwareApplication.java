ad087508c798c864a87f867513ed698047b49108
Tristan Elliott <>
2022-09-13 20:20:45 -0300

    initial commit

---
+++ src/main/java/elliott/software/RuralSoftware/RuralSoftwareApplication.java
@@ -0,0 +1,13 @@
+package elliott.software.RuralSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuralSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/elliott/software/RuralSoftware/RuralSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package elliott.software.RuralSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
