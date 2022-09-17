fa4c8347f9c08ce4d4663d9058345765194c717d
Tristan Elliott <>
2022-09-17 19:45:37 -0300

    adding the form for adding a calf

--- src/main/java/elliott/software/RuralSoftware/products/controllers/CalvingTrackerController.java
+++ src/main/java/elliott/software/RuralSoftware/products/controllers/CalvingTrackerController.java
@@ -1,16 +1,32 @@
 package elliott.software.RuralSoftware.products.controllers;
 
+import elliott.software.RuralSoftware.models.Calf;
+import elliott.software.RuralSoftware.models.CalfStatus;
+import elliott.software.RuralSoftware.products.services.CalfService;
+import elliott.software.RuralSoftware.repositories.CalfRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/products/CalvingTracker")
 public class CalvingTrackerController {
 
+    @Autowired
+    CalfService calfService;
+
     @GetMapping
-    public String calvingTracker(){
+    public String calvingTracker(Model model){
+
+        List<Calf> calfList = calfService.getAllCalves();
+
+        model.addAttribute("calves",calfList);
 
         return "calvingTracker";
     }
